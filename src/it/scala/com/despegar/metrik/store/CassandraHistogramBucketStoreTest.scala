5ccfd5c0fad9ee0a2abebf37d705204a5733ed7e
dberjman <dberjman@despegar.com>
2014-10-03 14:21:53 -0300

    integration test HistogramBucketStore

--- src/it/scala/com/despegar/metrik/store/CassandraHistogramBucketStoreTest.scala
+++ src/it/scala/com/despegar/metrik/store/CassandraHistogramBucketStoreTest.scala
@@ -1,6 +1,9 @@
 package com.despegar.metrik.store
 
+import com.despegar.metrik.model.HistogramBucket
+import org.HdrHistogram.Histogram
 import org.scalatest.{BeforeAndAfter, FunSuite}
+import scala.concurrent.duration._
 
 class CassandraHistogramBucketStoreTest extends FunSuite with BeforeAndAfter {
 
@@ -14,6 +17,8 @@ class CassandraHistogramBucketStoreTest extends FunSuite with BeforeAndAfter {
   })
 
   test("An Histogram should be capable of serialize and deserialize from Cassandra") {
+    val histograms = Seq(HistogramBucket(30, 30 seconds, new Histogram(3000000L, 3)))
+    CassandraHistogramBucketStore.store("testMetric",30 seconds, histograms)
     assert(1 == 1)
   }
 
--- src/main/scala/com/despegar/metrik/store/HistogramBucketStore.scala
+++ src/main/scala/com/despegar/metrik/store/HistogramBucketStore.scala
@@ -2,13 +2,13 @@ package com.despegar.metrik.store
 
 import java.nio.ByteBuffer
 
-import scala.concurrent.duration.Duration
 import com.despegar.metrik.model.HistogramBucket
 import scala.collection.JavaConverters._
 import org.HdrHistogram.Histogram
 import com.netflix.astyanax.model.ColumnFamily
 import com.netflix.astyanax.serializers.StringSerializer
 import com.netflix.astyanax.serializers.LongSerializer
+import scala.concurrent.duration._
 
 trait HistogramBucketStore {
 
@@ -19,12 +19,14 @@ trait HistogramBucketStore {
 }
 
 object CassandraHistogramBucketStore extends HistogramBucketStore {
-
-  val columnFamily = ColumnFamily.newColumnFamily("histogramBucket", StringSerializer.get(), LongSerializer.get())
+  //create column family definition for every bucket duration
+  val predefinedDurations :Seq[Duration] = Seq(0 seconds, 30 seconds, 1 minute, 5 minute, 10 minute, 30 minute, 1 hour) //FIXME put configured windows
+  val columnFamilis = predefinedDurations.map(duration => (duration, ColumnFamily.newColumnFamily(getColumnFamilyName(duration), StringSerializer.get(), LongSerializer.get()))).toMap
   val LIMIT = 1000
 
+  
   def sliceUntilNow(metric: String, windowDuration: Duration): Seq[HistogramBucket] = {
-    val result = Cassandra.keyspace.prepareQuery(columnFamily).getKey(getKey(metric, windowDuration)).withColumnRange(1L, System.currentTimeMillis(), false, LIMIT).execute()
+    val result = Cassandra.keyspace.prepareQuery(columnFamilis(windowDuration)).getKey(getKey(metric, windowDuration)).withColumnRange(1L, System.currentTimeMillis(), false, LIMIT).execute()
 
     result.getResult().asScala.map { column =>
       val timestamp = column.getName()
@@ -37,17 +39,21 @@ object CassandraHistogramBucketStore extends HistogramBucketStore {
 
   def store(metric: String, windowDuration: Duration, histogramBuckets: Seq[HistogramBucket]) = {
     val mutation = Cassandra.keyspace.prepareMutationBatch()
-    val colums = mutation.withRow(columnFamily, getKey(metric, windowDuration))
+    val colums = mutation.withRow(columnFamilis(windowDuration), getKey(metric, windowDuration))
     histogramBuckets.foreach( bucket => colums.putColumn(bucket.timestamp, serializeHistogram(bucket.histogram)))
 
     mutation.execute
   }
 
-  def getKey(metric: String, windowDuration: Duration): String = {
+  private def getColumnFamilyName(duration: Duration) = {
+    s"bucket${duration.length}${duration.unit}"
+  }
+
+  private def getKey(metric: String, windowDuration: Duration): String = {
     s"$metric.${windowDuration.length}${windowDuration.unit}"
   }
 
-  def serializeHistogram(histogram: Histogram) : ByteBuffer = {
+  private def serializeHistogram(histogram: Histogram) : ByteBuffer = {
     val buffer = ByteBuffer.allocate(histogram.getEstimatedFootprintInBytes)
     val bytesEncoded = histogram.encodeIntoCompressedByteBuffer(buffer)
 
