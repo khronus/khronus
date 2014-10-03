c9c0775cb37fa2c9e790eafb58ceb0d3263960aa
Pablo <pmedina@despegar.com>
2014-10-03 17:42:59 -0300

    Eclipse settings: resources and integration tests as sources

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,14 @@
 import sbt._
 import Keys._
+import com.typesafe.sbteclipse.plugin.EclipsePlugin._
 
 object B extends Build {
   lazy val root =
     Project("root", file(".")).
       configs( IntegrationTest ).
       settings( Defaults.itSettings : _*).
-      settings( libraryDependencies += specs )
+      settings( libraryDependencies += specs ).
+      settings(EclipseKeys.configurations := Set(Compile, Test, IntegrationTest), EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource)
 
   lazy val specs = "org.scalatest" %%  "scalatest" % "2.2.1" % "it,test"
 }
