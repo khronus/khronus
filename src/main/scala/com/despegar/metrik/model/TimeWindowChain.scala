310e424bdfaf9b22b89221a81ae47910079773f6
dberjman <demian.berjman@gmail.com>
2014-11-05 18:24:40 -0300

    continue on TimeWindowChainTest

--- src/main/scala/com/despegar/metrik/model/TimeWindowChain.scala
+++ src/main/scala/com/despegar/metrik/model/TimeWindowChain.scala
@@ -22,15 +22,14 @@ import com.despegar.metrik.util.{ BucketUtils, Logging, Settings }
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class TimeWindowChain extends Logging with MetaSupport {
+class TimeWindowChain extends TimeWindowsSupport with Logging with MetaSupport {
 
-  val histrogramsWindows = Settings().Histogram.timeWindows
-  val countersWindows = Settings().Counter.timeWindows
+  def mustExecute(timeWindow: TimeWindow[_,_], metric: Metric, timestampAligned: Long): Boolean = {
+    if ((timestampAligned % timeWindow.duration.toMillis) == 0) {
+      true
+    }
 
-  def filter(timeWindows: Seq[TimeWindow[_, _]], metric: Metric, alignedTimestamp: Long): Future[Seq[TimeWindow[_, _]]] = {
-    metaStore.getLastProcessedTimestamp(metric) map (lastProcessTS ⇒ {
-      timeWindows filter (timeWindow ⇒ (alignedTimestamp - lastProcessTS) >= timeWindow.duration.toMillis)
-    })
+    false
   }
 
   def process(metric: Metric): Future[Seq[Any]] = {
@@ -42,24 +41,26 @@ class TimeWindowChain extends Logging with MetaSupport {
       case "counter" ⇒ countersWindows
     }
 
-    val alignedTimestamp = BucketUtils.getCurrentBucketTimestamp(windows(0).duration, executionTimestamp)
-    
-    filter(windows, metric, alignedTimestamp) flatMap (filteredWindows ⇒ {
-      val sequence = Future.sequence(Seq(processInChain(filteredWindows, metric, executionTimestamp, 0)))
-      sequence onSuccess {
-        case _ ⇒ metaStore.update(metric, alignedTimestamp)
-      }
-      sequence
-    })
+    val timestampAligned = BucketUtils.getCurrentBucketTimestamp(windows(0).duration, executionTimestamp)
+
+    val sequence = Future.sequence(Seq(processInChain(windows filter (mustExecute(_, metric, timestampAligned)), metric, executionTimestamp, 0)))
+    sequence onSuccess {
+      case _ ⇒ metaStore.update(metric, timestampAligned)
+    }
+    sequence
   }
 
   def processInChain(windows: Seq[TimeWindow[_, _]], metric: Metric, executionTimestamp: Long, index: Int): Future[Unit] = {
-    if (index >= (windows.size - 1)) {
-      windows(index).process(metric, executionTimestamp)
-    } else {
-      windows(index).process(metric, executionTimestamp).flatMap { _ ⇒
-        processInChain(windows, metric, executionTimestamp, index + 1)
+    if (windows.size > 0 ) {
+      if (index >= (windows.size - 1)) {
+        windows(index).process(metric, executionTimestamp)
+      } else {
+        windows(index).process(metric, executionTimestamp).flatMap { _ ⇒
+          processInChain(windows, metric, executionTimestamp, index + 1)
+        }
       }
+    } else {
+      Future{}
     }
   }
 
--- src/test/scala/com/despegar/metrik/model/TimeWindowChainTest.scala
+++ src/test/scala/com/despegar/metrik/model/TimeWindowChainTest.scala
@@ -1,13 +1,34 @@
 package com.despegar.metrik.model
 
+import com.despegar.metrik.store.MetaStore
 import org.scalatest.FunSuite
 import org.scalatest.mock.MockitoSugar
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class TimeWindowChainTest  extends FunSuite with MockitoSugar {
 
   test("should execute TimeWindows that correspond to the tick time") {
-    val chain = new TimeWindowChain
+    val window30s = mock[HistogramTimeWindow]
+    val window1m = mock[HistogramTimeWindow]
 
+    val mockedWindows: Seq[HistogramTimeWindow] = Seq(window30s, window1m)
 
+    val chain = new TimeWindowChain {
+      override val histrogramsWindows = mockedWindows
+      override val metaStore = mock[MetaStore]
+    }
+
+    when(window30s.duration).thenReturn(30 seconds)
+    when(window1m.duration).thenReturn(1 minute)
+
+    when(chain.metaStore.update(any[Metric], any[Long])).thenReturn(Future{})
+
+    val result = chain.process(Metric("tito", "timer"))
+
+    Await.result(result, 5 seconds)
   }
 }
