59428d914be93a50d978581c4ef052ed0588c64e
Pablo <pmedina@despegar.com>
2014-11-12 15:23:02 -0300

    compression and test cases

--- src/main/scala/com/despegar/metrik/model/SkinnyHistogram.scala
+++ src/main/scala/com/despegar/metrik/model/SkinnyHistogram.scala
@@ -1,81 +1,118 @@
 package org.HdrHistogram
 
 import java.nio.ByteBuffer
+import java.util.zip.{Deflater, Inflater}
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{UnsafeInput, UnsafeOutput}
 
-class SkinnyHistogram(maxValue: Long, precision: Int) extends Histogram(maxValue, precision) {
+class SkinnyHistogram(lowestValue: Long, maxValue: Long, precision: Int) extends Histogram(lowestValue, maxValue, precision) {
 
-  val encodingCookieBase = 0x1c849308
-  
-  override def encodeIntoByteBuffer(buffer: ByteBuffer): Int = {
-    val (idxArray, freqArray) = createArrays
-
-    val output = new UnsafeOutput(buffer.array())
-    output.supportVarInts(true)
-    output.writeVarInt(0x1c849308+ (wordSizeInBytes << 4), true)
-    output.writeVarInt(numberOfSignificantValueDigits.toInt, true)
-    output.writeVarLong(lowestTrackableValue, true)
-    output.writeVarLong(highestTrackableValue, true)
-    output.writeVarLong(getTotalCount, true)
-    output.writeVarInt(idxArray.length, true)
-    idxArray.zip(freqArray) foreach { tuple =>
-      val idx = tuple._1
-      val freq = tuple._2
-      output.writeVarInt(idx, true)
-      output.writeVarInt(freq.toInt, false)
-    }
-    output.flush()
-    output.total().toInt
+  def this(maxValue: Long, precision: Int)  {
+    this(1L, maxValue, precision)
   }
 
 
-  private def createArrays: (Array[Int], Array[Long]) = {
-    var idxArray = Seq[Int]()
-    var freqArray = Seq[Long]()
+  override def encodeIntoCompressedByteBuffer(targetBuffer: ByteBuffer): Int = {
+    val intermediateUncompressedByteBuffer = ByteBuffer.allocate(this.getNeededByteBufferCapacity())
+    val uncompressedLength = this.encodeIntoByteBuffer(intermediateUncompressedByteBuffer)
+
+    targetBuffer.putInt(SkinnyHistogram.encodingCompressedCookieBase)
+    targetBuffer.putInt(0)
+    targetBuffer.putInt(uncompressedLength)
+    val compressor = new Deflater(SkinnyHistogram.defaultCompressionLevel)
+    compressor.setInput(intermediateUncompressedByteBuffer.array(), 0, uncompressedLength)
+    compressor.finish()
+    val targetArray = targetBuffer.array()
+    val compressedDataLength = compressor.deflate(targetArray, 12, targetArray.length - 12)
+    compressor.end()
+    targetBuffer.putInt(4, compressedDataLength)
+    compressedDataLength + 12
+  }
+
+  override def encodeIntoByteBuffer(buffer: ByteBuffer): Int = {
+    val output = new UnsafeOutput(buffer.array())
+    output.supportVarInts(true)
+    output.writeVarInt(numberOfSignificantValueDigits, true)
+    output.writeVarLong(lowestTrackableValue, true)
+    output.writeLong(highestTrackableValue)
+    output.writeVarLong(getTotalCount, true)
+
+    val countsDiffsSeq = countsDiffs
+    output.writeVarInt(countsDiffsSeq.length, true)
+    countsDiffsSeq foreach { tuple =>
+      val idx = tuple._1
+      val freq = tuple._2
+      output.writeVarInt(idx, true)
+      output.writeVarLong(freq, false)
+    }
+    output.flush()
+    val total = output.total().toInt
+    output.close()
+    total
+  }
+
+  private def countsDiffs: Seq[(Int,Long)] = {
+    var seq = Seq[(Int,Long)]()
     var lastValue: Long = 0
     var lastIdx: Int = 0
-    
     for ( i <- (0 to  (counts.length - 1)) ) {
-        val (idx, value) = (i, counts(i))
-        if (value > 0) {
-        idxArray = (idxArray :+ (idx - lastIdx)).asInstanceOf[Seq[Int]]
-        freqArray = (freqArray :+ (value - lastValue) ).asInstanceOf[Seq[Long]]
+      val (idx, value) = (i, counts(i))
+      if (value > 0) {
+        seq = seq :+ ((idx - lastIdx), (value - lastValue))
         lastIdx = idx
         lastValue = value
       }
     }
-    (idxArray.toArray, freqArray.toArray)
+    seq
   }
 
 }
 
 object SkinnyHistogram {
-  def decodeFromByteBuffer(buffer: ByteBuffer): SkinnyHistogram = {
-    val input = new UnsafeInput(buffer.array())
+  private val encodingCookieBase:Int = 129
+  private val encodingCompressedCookieBase:Int = 130
+  private val defaultCompressionLevel = -1
 
-    val cookie = input.readVarInt(true)
+  def decodeFromCompressedByteBuffer(buffer: ByteBuffer, minBarForHighestTrackableValue: Long): Histogram = {
+    val cookie = buffer.getInt()
+    if (cookie != encodingCompressedCookieBase) {
+      buffer.rewind()
+      return Histogram.decodeFromCompressedByteBuffer(buffer, minBarForHighestTrackableValue)
+    }
+    val lengthOfCompressedContents = buffer.getInt()
+    val lengthOfUnCompressedContents = buffer.getInt()
+
+    val decompressor = new Inflater()
+    decompressor.setInput(buffer.array(), 12, lengthOfCompressedContents);
+    val decompressedBuffer = ByteBuffer.allocate(lengthOfUnCompressedContents);
+    decompressor.inflate(decompressedBuffer.array());
+
+    return decodeFromByteBuffer(decompressedBuffer)
+  }
+
+  def decodeFromByteBuffer(buffer: ByteBuffer): Histogram = {
+    val input = new UnsafeInput(buffer.array(),0,buffer.limit())
+    input.setVarIntsEnabled(true)
     val significantValueDigits = input.readVarInt(true)
     val lowest = input.readVarLong(true)
-    val highest = input.readVarLong(true)
+    val highest = input.readLong()
     val totalCount = input.readVarLong(true)
-
     val idxArrayLength = input.readVarInt(true)
 
-    val skinnyHistogram = new SkinnyHistogram(highest, significantValueDigits)
-
+    val skinnyHistogram = new SkinnyHistogram(lowest, highest, significantValueDigits)
+    skinnyHistogram.clearCounts()
     var lastIdx = 0
-    var lastFreq = 0
-    (1 to idxArrayLength) foreach { a =>
+    var lastFreq = 0L
+    (1 to idxArrayLength) foreach { _ =>
       val idx = input.readVarInt(true) + lastIdx
-      val freq = input.readVarInt(false) + lastFreq
-
+      val freq = input.readVarLong(false) + lastFreq
       skinnyHistogram.addToCountAtIndex(idx, freq)
-
       lastIdx = idx
       lastFreq = freq
     }
+    skinnyHistogram.setTotalCount(totalCount)
 
     skinnyHistogram
   }
+
 }
--- src/test/scala/com/despegar/metrik/model/HistogramTest.scala
+++ src/test/scala/com/despegar/metrik/model/HistogramTest.scala
@@ -17,11 +17,11 @@ object HistogramTest extends App {
     val onePercentError = 2
 
     val histogram0 = new Histogram(oneHourInMicroseconds, 2)
-    val histogram1 = new Histogram(3600000L, 3)
+    val histogram1 = new Histogram(oneHourInMicroseconds, 3)
     val histogram2 = new SkinnyHistogram(oneHourInMicroseconds, 3)
     val histogram3 = new SkinnyHistogram(3600000L, onePercentError)
     
-    for (i <- (1 to 100000)) {
+    for (i <- (1 to 1000000)) {
       val r = Random.nextInt(250)
       histogram0.recordValue(r)
       histogram1.recordValue(r)
@@ -32,34 +32,17 @@ object HistogramTest extends App {
     val b = serializeBucket(histogram1)
     val c = serializeBucket(histogram2)
     val d = serializeBucket(histogram3)
+
     println(s"Reduction: ${100 - (c.toDouble / b.toDouble) * 100}%")
-//    b
   }
 
   def serializeBucket(histo: Histogram): Int = {
-    val buffer = ByteBuffer.allocate(histo.getEstimatedFootprintInBytes)
-    val bytesEncoded = histo.encodeIntoCompressedByteBuffer(buffer) //TODO: Find a better way to do this serialization
-    println(s"histo: $histo serialized into $bytesEncoded")
-    buffer.limit(bytesEncoded)
-    buffer.rewind()
-    val byteArray = new Array[Byte](bytesEncoded)
-    val byteArray2 = new Array[Byte](bytesEncoded)
-    buffer.get(byteArray)
-    val compressed = Snappy.compress(byteArray)
-    bytesEncoded
+    val encodedBytes = histo.encodeIntoByteBuffer(ByteBuffer.allocate(histo.getEstimatedFootprintInBytes))
+    val compressedBytes = histo.encodeIntoCompressedByteBuffer(ByteBuffer.allocate(histo.getEstimatedFootprintInBytes))
+    println(s"$histo serialized into $compressedBytes")
+    println(s"Raw: $encodedBytes, deflated: $compressedBytes")
 
-
-    val compressor = new Deflater(9);
-    compressor.setInput(byteArray, 0, bytesEncoded);
-    compressor.finish();
-    val compressedDataLength = compressor.deflate(byteArray2, 0, bytesEncoded);
-    compressor.end();
-    //compressed.length
-
-    println(s"kryo: $bytesEncoded, snappy: ${compressed.length} and deflater: $compressedDataLength")
-
-    bytesEncoded
+    compressedBytes
   }
 
-  private def deserializeHistogram(bytes: ByteBuffer): Histogram = Histogram.decodeFromCompressedByteBuffer(bytes, 0)
 }
