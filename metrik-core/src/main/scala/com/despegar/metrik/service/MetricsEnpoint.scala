93cdc019217e33e589c10219ada597695fd2b14e
Pablo <pmedina@despegar.com>
2014-12-09 13:40:43 -0300

    Measure spray deserialization

--- metrik-core/src/main/scala/com/despegar/metrik/service/MetricsEnpoint.scala
+++ metrik-core/src/main/scala/com/despegar/metrik/service/MetricsEnpoint.scala
@@ -7,7 +7,10 @@ import com.despegar.metrik.store.MetricMeasurementStoreSupport
 import com.despegar.metrik.util.log.Logging
 import spray.http.StatusCodes._
 import spray.httpx.encoding.{ Gzip, NoEncoding }
-import spray.routing.{ HttpService, HttpServiceActor, Route }
+import spray.routing._
+
+import scala.util.Failure
+import scala.util.control.NonFatal
 
 class MetrikActor extends HttpServiceActor with MetricsEnpoint with MetrikHandlerException {
   def receive = runRoute(metricsRoute)
@@ -22,18 +25,57 @@ object MetrikActor {
 
 trait MetricsEnpoint extends HttpService with MetricMeasurementStoreSupport with Logging {
 
+  import com.despegar.metrik.service.SprayMetrics._
+
   override def loggerName = classOf[MetricsEnpoint].getName()
 
   val metricsRoute: Route =
     decompressRequest(Gzip, NoEncoding) {
       post {
-        entity(as[MetricBatch]) { metricBatch ⇒
-          complete {
-            metricStore.storeMetricMeasurements(metricBatch.metrics)
-            OK
+        time("sprayTimeWithDeserialization") {
+          entity(as[MetricBatch]) { metricBatch ⇒
+            time("sprayTimeWithoutDeserialization") {
+              complete {
+                val start = System.currentTimeMillis()
+                metricStore.storeMetricMeasurements(metricBatch.metrics)
+                log.info(s"storeMetricMeasurements time spent ${System.currentTimeMillis() - start} ms")
+                OK
+              }
+            }
           }
         }
       }
+
+    }
+
+}
+
+object SprayMetrics extends Logging {
+
+  import spray.routing.directives.BasicDirectives._
+
+  def around(before: RequestContext ⇒ (RequestContext, Any ⇒ Any)): Directive0 =
+    mapInnerRoute { inner ⇒
+      ctx ⇒
+        val (ctxForInnerRoute, after) = before(ctx)
+        try inner(ctxForInnerRoute.withRouteResponseMapped(after))
+        catch {
+          case NonFatal(ex) ⇒ after(Failure(ex))
+        }
+    }
+
+  def buildAfter(name: String, start: Long): Any ⇒ Any = { possibleRsp: Any ⇒
+    possibleRsp match {
+      case _ ⇒
+        log.info(s"$name time spent ${System.currentTimeMillis() - start} ms")
+    }
+    possibleRsp
+  }
+
+  def time(name: String): Directive0 =
+    around { ctx ⇒
+      val timerContext = System.currentTimeMillis()
+      (ctx, buildAfter(name, timerContext))
     }
 
 }
