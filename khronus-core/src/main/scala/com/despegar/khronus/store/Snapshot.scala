a507a4359af479390a87094af9b8effdac6a8df5
Pablo <pmedina@despegar.com>
2014-12-11 11:53:12 -0300

    Keep snapshot reloader working in face of exceptions

--- khronus-core/src/main/scala/com/despegar/khronus/store/Snapshot.scala
+++ khronus-core/src/main/scala/com/despegar/khronus/store/Snapshot.scala
@@ -1,28 +1,34 @@
 package com.despegar.khronus.store
 
-import java.util.concurrent.{ Executors, TimeUnit }
+import java.util.concurrent.TimeUnit
 
 import com.despegar.khronus.util.ConcurrencySupport
+import com.despegar.khronus.util.log.Logging
 
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.{ Failure, Success }
-import com.despegar.khronus.util.log.Logging
 
 trait Snapshot[T] extends Logging with ConcurrencySupport {
 
   implicit def context: ExecutionContext
-  val snapshotName: String
 
+  def snapshotName: String
+
+  @volatile
   var snapshot: T = _
 
-  private val pool = scheduledThreadPool(s"$snapshotName-snapshot-reload-worker", 1)
-  pool.scheduleAtFixedRate(reload(), 1, 5, TimeUnit.SECONDS)
+  private val pool = scheduledThreadPool(s"$snapshotName-snapshot-reload-worker")
+  pool.scheduleAtFixedRate(reload(), 0, 5, TimeUnit.SECONDS)
 
   private def reload() = new Runnable {
     override def run(): Unit = {
-      getFreshData() onComplete {
-        case Success(data) ⇒ snapshot = data
-        case Failure(t)    ⇒ log.error("Error reloading data", t)
+      try {
+        getFreshData() onComplete {
+          case Success(data) ⇒ snapshot = data
+          case Failure(t)    ⇒ log.error("Error reloading data", t)
+        }
+      } catch {
+        case reason: Throwable ⇒ log.error("Unexpected error reloading data", reason)
       }
     }
   }
