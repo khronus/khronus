2868e33f5095d5da9f46092bbbcdd17e1d347b7e
Pablo <pmedina@despegar.com>
2015-11-04 01:41:45 -0300

    optimize logging

--- khronus-core/src/main/scala/com/searchlight/khronus/store/Cassandra.scala
+++ khronus-core/src/main/scala/com/searchlight/khronus/store/Cassandra.scala
@@ -191,7 +191,7 @@ trait CassandraUtils extends Logging {
     }
   }
 
-  final def executeChunked[T](msg: String, items: Seq[T], chunkSize: Int)(block: Seq[T] ⇒ Future[Unit])(implicit ec: ExecutionContext): Future[Unit] = {
+  final def executeChunked[T](msg: ⇒ String, items: Seq[T], chunkSize: Int)(block: Seq[T] ⇒ Future[Unit])(implicit ec: ExecutionContext): Future[Unit] = {
     if (items.nonEmpty) {
       val futures = items.grouped(chunkSize).map(chunk ⇒ block(chunk))
       Future.sequence(futures).andThen {
