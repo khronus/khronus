660c1ca8b3a7b297c760f5fa970a91b28c2d21e5
Pablo <pmedina@despegar.com>
2016-02-08 20:59:55 -0300

    histogram dynamic query test

--- khronus-core/src/main/scala/com/searchlight/khronus/query/Projection.scala
+++ khronus-core/src/main/scala/com/searchlight/khronus/query/Projection.scala
@@ -1,43 +1,59 @@
 package com.searchlight.khronus.query
 
 import com.searchlight.khronus.model._
-import scala.concurrent.duration._
-import scala.concurrent.Future
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
 
 trait Projection {
   def execute(input: Map[QMetric, Map[SubMetric, Future[BucketSlice[Bucket]]]]): Option[Future[Seq[Series]]] = {
-    subMetrics(input).map { subMetrics ⇒
-      val res = Future.sequence(subMetrics.map { case (subMetric, futures) ⇒ futures.map(buckets ⇒ (subMetric, buckets)) })
-      res.map { entries ⇒
-        aggregate(entries.flatMap(_._2.results).toSeq.groupBy(_.timestamp.toBucketNumberOf(1 minute))).toSeq.map {
-          kv ⇒
-            value(kv._2).map(value ⇒ Point(kv._1.startTimestamp(), value))
-        }.map(points ⇒ Series(alias, points))
+    targetQMetric(input).flatMap { qMetric ⇒
+      input.get(qMetric).map { subMetrics ⇒
+        bucketSlices(subMetrics).map { bucketSlices ⇒
+          aggregate(subMetrics.keySet.head.asMetric(), groupedBuckets(bucketSlices)).toSeq.flatMap {
+            case (bucketNumber, bucket) ⇒
+              values(bucket).map(pair ⇒ (pair._1, Point(bucketNumber.startTimestamp(), pair._2)))
+          }.groupBy {
+            _._1
+          }.map { case (projectionName, points) ⇒ Series(s"${qMetric.name}.$projectionName", points.map(_._2)) }.toSeq
+        }
       }
     }
   }
 
-  protected def value(bucket: Bucket): Seq[Double] = {
+  private def groupedBuckets(bucketSlices: Iterable[(SubMetric, BucketSlice[Bucket])]): Map[BucketNumber, Seq[BucketResult[Bucket]]] = {
+    bucketSlices.flatMap(_._2.results).toSeq.groupBy(_.timestamp.toBucketNumberOf(1 minute))
+  }
+
+  private def bucketSlices(subMetrics: Map[SubMetric, Future[BucketSlice[Bucket]]]): Future[Iterable[(SubMetric, BucketSlice[Bucket])]] = {
+    Future.sequence(subMetrics.map { case (subMetric, futures) ⇒ futures.map(buckets ⇒ (subMetric, buckets)) })
+  }
+
+  protected def values(bucket: Bucket): Seq[(String, Double)] = {
     bucket match {
-      case counter: CounterBucket     ⇒ value(counter)
-      case histogram: HistogramBucket ⇒ value(histogram)
+      case counter: CounterBucket     ⇒ values(counter)
+      case histogram: HistogramBucket ⇒ values(histogram)
     }
   }
 
-  protected def value(bucket: CounterBucket): Seq[Double]
+  protected def values(bucket: CounterBucket): Seq[(String, Double)]
 
-  protected def value(bucket: HistogramBucket): Seq[Double]
+  protected def values(bucket: HistogramBucket): Seq[(String, Double)]
 
-  private def aggregate(bucketsMap: Map[BucketNumber, Seq[BucketResult[Bucket]]]): Map[BucketNumber, Bucket] = {
-    bucketsMap.map { case (bucketNumber, buckets) ⇒ (bucketNumber, aggregate(bucketNumber, buckets)) }
+  private def aggregate(metric: Metric, bucketsMap: Map[BucketNumber, Seq[BucketResult[Bucket]]]): Map[BucketNumber, Bucket] = {
+    bucketsMap.map { case (bucketNumber, buckets) ⇒ (bucketNumber, aggregate(metric, bucketNumber, buckets)) }
   }
 
-  private def aggregate(number: BucketNumber, buckets: Seq[BucketResult[Bucket]]): Bucket = {
-    buckets match {
-      case counters: Seq[BucketResult[CounterBucket]]     ⇒ new CounterBucket(number, CounterBucket.sumCounters(counters.map(_.lazyBucket())))
-      case histograms: Seq[BucketResult[HistogramBucket]] ⇒ new HistogramBucket(number, HistogramBucket.sumHistograms(histograms.map(_.lazyBucket())))
+  private def aggregate(metric: Metric, number: BucketNumber, buckets: Seq[BucketResult[Bucket]]): Bucket = {
+    metric.mtype match {
+      case "counter"                       ⇒ new CounterBucket(number, CounterBucket.sumCounters(buckets.map(_.lazyBucket().asInstanceOf[CounterBucket])))
+      case "timer" | "histogram" | "gauge" ⇒ new HistogramBucket(number, HistogramBucket.sumHistograms(buckets.map(_.lazyBucket().asInstanceOf[HistogramBucket])))
     }
+  }
+
+  private def targetQMetric(input: Map[QMetric, Map[SubMetric, Future[BucketSlice[Bucket]]]]) = {
+    input.find { case (qMetric, subMetrics) ⇒ qMetric.alias.equals(alias) }.map(_._1)
   }
 
   def subMetrics(input: Map[QMetric, Map[SubMetric, Future[BucketSlice[Bucket]]]]): Option[Map[SubMetric, Future[BucketSlice[Bucket]]]] = {
@@ -49,13 +65,13 @@ trait Projection {
 }
 
 case class Count(alias: String) extends Projection {
-  def value(counter: CounterBucket) = Seq(counter.counts)
+  def values(counter: CounterBucket) = Seq(("count", counter.counts.toDouble))
 
-  def value(histogram: HistogramBucket) = Seq(histogram.histogram.getTotalCount)
+  def values(histogram: HistogramBucket) = Seq(("count", histogram.histogram.getTotalCount.toDouble))
 }
 
 case class Percentiles(alias: String, percentiles: Seq[Double]) extends Projection {
-  def value(counter: CounterBucket) = percentiles.map(p ⇒ 0d)
+  def values(counter: CounterBucket) = percentiles.map(p ⇒ ("undefined", 0d))
 
-  def value(histogram: HistogramBucket) = percentiles.map(percentile ⇒ histogram.histogram.getValueAtPercentile(percentile).toDouble)
+  def values(histogram: HistogramBucket) = percentiles.map(percentile ⇒ (s"p$percentile", histogram.histogram.getValueAtPercentile(percentile).toDouble))
 }
--- khronus-core/src/test/scala/com/searchlight/khronus/query/DynamicQueryExecutorTest.scala
+++ khronus-core/src/test/scala/com/searchlight/khronus/query/DynamicQueryExecutorTest.scala
@@ -1,49 +1,85 @@
 package com.searchlight.khronus.query
 
 import com.searchlight.khronus.model._
+import org.HdrHistogram.Histogram
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{ FunSuite, Matchers }
-
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, Future }
 
 class DynamicQueryExecutorTest extends FunSuite with Matchers with MockitoSugar {
 
+  val someEvent_qmetric: QMetric = QMetric("some_event", "e")
+  val someEvent_metric: Metric = Metric("some_event", "counter")
+  val someEvent_submetric1 = SubMetric(someEvent_metric, Map("tag1" -> "someValue", "tag2" -> "otherValue1"))
+  val someEvent_submetric2 = SubMetric(someEvent_metric, Map("tag1" -> "someValue", "tag2" -> "otherValue2"))
+
+  val someTimer_qmetric: QMetric = QMetric("some_timer", "t")
+  val someTimer_metric: Metric = Metric("some_timer", "histogram")
+  val someTimer_submetric1 = SubMetric(someTimer_metric, Map("tag1" -> "someValue", "tag2" -> "otherValue1"))
+  val someTimer_submetric2 = SubMetric(someTimer_metric, Map("tag1" -> "someValue", "tag2" -> "otherValue2"))
+
   test("select count(e) from some_event e where e.tag1 = 'someValue' ") {
-    val someEvent: QMetric = QMetric("some_event", "e")
     val query: DynamicQuery = DynamicQuery(Seq(Count("e")),
-      Seq(someEvent), Equals("e", "tag1", "someValue"), TimeRange(Timestamp(1), Timestamp(2)))
+      Seq(someEvent_qmetric), Equals("e", "tag1", "someValue"), TimeRange(Timestamp(1), Timestamp(2)))
 
     val metricSelectorMock: QueryPlanner = mock[QueryPlanner]
-
-    val metric: Metric = Metric("some_event", "counter")
-    val subMetric1 = SubMetric(metric, Map("tag1" -> "someValue", "tag2" -> "otherValue1"))
-    val subMetric2 = SubMetric(metric, Map("tag1" -> "someValue", "tag2" -> "otherValue2"))
-
     when(metricSelectorMock.getQueryPlan(query)).thenReturn(
-      QueryPlan(Map(someEvent -> Seq(subMetric1, subMetric2))))
+      QueryPlan(Map(someEvent_qmetric -> Seq(someEvent_submetric1, someEvent_submetric2))))
 
-    val executor: BucketQueryExecutor = mock[BucketQueryExecutor]
+    val bucketQueryExecutorMock: BucketQueryExecutor = mock[BucketQueryExecutor]
     val counterBucket: CounterBucket = new CounterBucket(BucketNumber(1L, 1 minute), 1L)
-    when(executor.retrieve(subMetric1, query.range)).thenReturn(Future.successful(BucketSlice(counterBucket)))
-    when(executor.retrieve(subMetric2, query.range)).thenReturn(Future.successful(BucketSlice(counterBucket)))
+    when(bucketQueryExecutorMock.retrieve(someEvent_submetric1, query.range)).thenReturn(Future.successful(BucketSlice(counterBucket)))
+    when(bucketQueryExecutorMock.retrieve(someEvent_submetric2, query.range)).thenReturn(Future.successful(BucketSlice(counterBucket)))
 
-    trait BucketQueryExecutorMockSupport extends BucketQuerySupport {
-      override val bucketQueryExecutor = executor
-    }
-
-    val dynamicQueryExecutor = new DefaultDynamicQueryExecutor with BucketQueryExecutorMockSupport {
+    val dynamicQueryExecutor = new DefaultDynamicQueryExecutor {
       override val queryPlanner = metricSelectorMock
+      override val bucketQueryExecutor = bucketQueryExecutorMock
     }
+
     val future = dynamicQueryExecutor.execute(query)
 
     val series = Await.result(future, 3 seconds)
 
     series should have size 1
-    series.head.name should equal("e")
+    series.head.name should equal("some_event.count")
     series.head.points should equal(Seq(Point(Timestamp(60000), 2d)))
+  }
 
+  test("select percentiles(t, 20, 50, 95, 99.9) from some_timer t where e.tag1 = 'someValue' ") {
+    val query: DynamicQuery = DynamicQuery(Seq(Percentiles("t", Seq(20, 50, 95, 99.9))),
+      Seq(someTimer_qmetric), Equals("t", "tag1", "someValue"), TimeRange(Timestamp(1), Timestamp(2)))
+
+    val metricSelectorMock: QueryPlanner = mock[QueryPlanner]
+    when(metricSelectorMock.getQueryPlan(query)).thenReturn(
+      QueryPlan(Map(someTimer_qmetric -> Seq(someTimer_submetric1, someTimer_submetric2))))
+
+    val bucketQueryExecutorMock: BucketQueryExecutor = mock[BucketQueryExecutor]
+    val histo1 = new Histogram(1000, 3)
+    (1 to 500) foreach (n ⇒ histo1.recordValue(n))
+    val histo2 = new Histogram(1000, 3)
+    (501 to 1000) foreach (n ⇒ histo2.recordValue(n))
+    val bucket1: HistogramBucket = new HistogramBucket(BucketNumber(1L, 1 minute), histo1)
+    val bucket2: HistogramBucket = new HistogramBucket(BucketNumber(1L, 1 minute), histo2)
+
+    when(bucketQueryExecutorMock.retrieve(someTimer_submetric1, query.range)).thenReturn(Future.successful(BucketSlice(bucket1)))
+    when(bucketQueryExecutorMock.retrieve(someTimer_submetric2, query.range)).thenReturn(Future.successful(BucketSlice(bucket2)))
+
+    val dynamicQueryExecutor = new DefaultDynamicQueryExecutor {
+      override val queryPlanner = metricSelectorMock
+      override val bucketQueryExecutor = bucketQueryExecutorMock
+    }
+
+    val future = dynamicQueryExecutor.execute(query)
+
+    val series = Await.result(future, 3 seconds)
+
+    series should have size 4
+    series should contain(Series("some_timer.p20.0", Seq(Point(Timestamp(60000), 200))))
+    series should contain(Series("some_timer.p50.0", Seq(Point(Timestamp(60000), 500))))
+    series should contain(Series("some_timer.p95.0", Seq(Point(Timestamp(60000), 950))))
+    series should contain(Series("some_timer.p99.9", Seq(Point(Timestamp(60000), 999))))
   }
 
 }
