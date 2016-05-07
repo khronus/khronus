96676c9770642f1a8cff6ff956005341672a52d7
Pablo <pmedina@despegar.com>
2016-05-07 17:53:17 -0300

    increase test timeout

--- khronus-core/src/test/scala/com/searchlight/khronus/cluster/MasterSpec.scala
+++ khronus-core/src/test/scala/com/searchlight/khronus/cluster/MasterSpec.scala
@@ -50,7 +50,7 @@ class MasterSpec extends TestKitBase with ImplicitSender
       |}
     """.stripMargin))
 
-  val timeout: FiniteDuration = 1500 millis
+  val timeout: FiniteDuration = 5000 millis
   override protected def afterAll() = TestKit.shutdownActorSystem(system)
 
   "The Master actor" should {
