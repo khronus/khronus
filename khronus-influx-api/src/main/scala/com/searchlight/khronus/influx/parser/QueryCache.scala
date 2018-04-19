34eed6216bbb376beed29fb0be57aa803c400cc3
Demian Berjman <demian.berjman@gmail.com>
2018-04-19 19:16:49 -0300

    fix parse query in cache

--- khronus-influx-api/src/main/scala/com/searchlight/khronus/influx/parser/QueryCache.scala
+++ khronus-influx-api/src/main/scala/com/searchlight/khronus/influx/parser/QueryCache.scala
@@ -8,7 +8,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 trait QueryCache extends Logging {
-  val regexTime = ".*now.{2}-([0-9]+)([a-z]+) group.*".r
+  val regexWitNames = new util.matching.Regex(".*(now.{2}-([0-9]+)([a-z]+)|> ([0-9]+)(s) and time < ([0-9]+)(s)) group.*", "all", "relativeTime", "relativeSuffix", "secondsGreater", "greaterSuffix", "secondsMinor", "minorSuffix")
 
   private def toMillis(number: String, suffix: String): Long = suffix match {
     case TimeSuffixes.Seconds ⇒ TimeUnit.SECONDS.toMillis(number.toLong)
@@ -19,22 +19,18 @@ trait QueryCache extends Logging {
     case _                    ⇒ number.toLong
   }
 
-  def delta(now: Long, query: String): Long = {
-    now - (query match {
-      case regexTime(time, preffix) ⇒ toMillis(time, preffix)
-      case _                        ⇒ 0
-    })
+  def delta(now: Long, relativeTime: String, relativeSuffix: String): Long = {
+    now - toMillis(relativeTime, relativeSuffix)
   }
 
   def putInCache(query: String, parsedQuery: InfluxCriteria, now: Long)(implicit cache: scala.collection.concurrent.Map[String, (Boolean, InfluxCriteria)]): Boolean = {
-    val timeFilters = parsedQuery.filters.filter(_.isInstanceOf[TimeFilter])
-    if (timeFilters.size != 1) {
-      log.info(s"Query has more than one time filter and not apply for cache: $query")
-      return false
-    }
+    //    val timeFilters = parsedQuery.filters.filter(_.isInstanceOf[TimeFilter])
+    //    if (timeFilters.size != 1) {
+    //      log.info(s"Query has more than one time filter and not apply for cache: $query")
+    //      return false
+    //    }
 
-    val timeFilter = timeFilters.last.asInstanceOf[TimeFilter]
-    val cacheCandidate = parsedQuery.copy(filters = Seq(timeFilter.copy(value = delta(now, query))))
+    val cacheCandidate: InfluxCriteria = refreshTimeFilter(query, parsedQuery, now)
     if (cacheCandidate.equals(parsedQuery)) {
       cache.put(query, (true, parsedQuery))
       return true
@@ -43,6 +39,24 @@ trait QueryCache extends Logging {
       log.info(s"Query not apply for cache for unknown reasons: $query")
       return false
     }
+  }
+
+  private def refreshTimeFilter(query: String, parsedQuery: InfluxCriteria, now: Long): InfluxCriteria = {
+    val timeFilters = parsedQuery.filters.filter(_.isInstanceOf[TimeFilter])
+
+    val groups = regexWitNames.findFirstMatchIn(query).get
+
+    if (timeFilters.lengthCompare(1) == 0) {
+      val timeFilter = timeFilters.last.asInstanceOf[TimeFilter]
+      return parsedQuery.copy(filters = Seq(timeFilter.copy(value = delta(now, groups.group("relativeTime"), groups.group("relativeSuffix")))))
+    } else if (timeFilters.lengthCompare(2) == 0) {
+      val timeFilterGreater = timeFilters(0).asInstanceOf[TimeFilter]
+      val timeFilterMinor = timeFilters(1).asInstanceOf[TimeFilter]
+      return parsedQuery.copy(filters = Seq(timeFilterGreater.copy(value = toMillis(groups.group("secondsGreater"), groups.group("greaterSuffix"))),
+        timeFilterMinor.copy(value = toMillis(groups.group("secondsMinor"), groups.group("minorSuffix")))))
+    }
+
+    throw new UnsupportedOperationException(s"not support query cache for $query")
   }
 
   def cacheQuery(query: String)(block: Long ⇒ Future[InfluxCriteria])(implicit cache: scala.collection.concurrent.Map[String, (Boolean, InfluxCriteria)]): Future[InfluxCriteria] = {
@@ -55,7 +69,7 @@ trait QueryCache extends Logging {
         })
       }
 
-      case Some((isValid, parsedQuery)) ⇒ if (isValid) Future.successful(parsedQuery) else block(now)
+      case Some((isValid, cacheQuery)) ⇒ if (isValid) Future.successful(refreshTimeFilter(query, cacheQuery, now)) else block(now)
     }
   }
 }
